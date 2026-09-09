abd8160b2d93df10c03fc414cff100302f04ba72
Daniel Viola <daniel.viola001@mymdc.net>
2026-09-09 13:14:59 -0400

    e

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const program = new Command();
 program
   .name("time-traveler")
   .description("Turn your Git history into music")
-  .version("0.1.0");
+  .version("0.1.1");
 
 registerAnalyzeCommand(program);
 registerExportCommand(program);
